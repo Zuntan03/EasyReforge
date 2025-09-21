2741d3bf7b9451be1312db1c33b58857d480d6f9
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2025-09-21 17:48:04 +0900

    Download\Stable-diffusion\NoobV\ObsessionV_v20.bat のバージョンを更新しました。

--- Download/Stable-diffusion/NoobV/ObsessionV_v20.bat
+++ Download/Stable-diffusion/NoobV/ObsessionV_v20.bat
@@ -4,8 +4,8 @@ set EASY_TOOLS=%~dp0..\..\..\EasyTools
 set CIVITAI_MODEL=%EASY_TOOLS%\Download\CivitaiModel.bat
 pushd %~dp0..\..\..\Model\Stable-diffusion
 
-@REM https://civitai.com/models/820208?modelVersionId=1494740
-call %CIVITAI_MODEL% NoobV\ ObsessionV_v11.safetensors 820208 1494740
+@REM https://civitai.com/models/820208?modelVersionId=2234052
+call %CIVITAI_MODEL% NoobV\ ObsessionV_v20.safetensors 820208 2234052
 if %ERRORLEVEL% neq 0 ( popd & exit /b 1 )
 
 popd rem %~dp0..\..\..\Model\Stable-diffusion
